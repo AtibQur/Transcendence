15b86f6b79e1c3ac3d02aeea05cdcfc70e429fac
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2023-05-08 14:11:28 +0200

    front and back dependent on eacherother but not yet in container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule} from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ // New
-      rootPath: join(__dirname, '../..', '/client/dist'), // New
+      rootPath: join(__dirname, '../..', '/frontend/dist'), // New
     }), // New
   ],
  controllers: [AppController],
