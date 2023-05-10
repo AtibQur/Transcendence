890af91142b2fd669e0878016791b22724bbf552
Haseeb Qureshie <haseeb@MacBook-Air-van-Haseeb.local>
2023-05-10 15:00:52 +0200

    Added new components and in the middle of setting up routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/images/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <AppHome />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/common//HelloWorld.vue';
+
+import Home from './components/views/Home.vue';
 
 export default defineComponent({
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    AppHome: Home,
   }
 });
 </script>
 
 <style>
 #app {
+  background-color: #60b2c4;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #27425c;
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import App from './App.vue';
+import Home from './components/views/Home.vue';
+import About from './components/views/About.vue';
+
+const routes: RouteRecordRaw[] = [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/about',
+      component: About,
+    },
+  ];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
 
 createApp(App).mount('#app')
