79e3065bd604d64f7c3be940741a997aab5685b2
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2023-05-10 17:59:28 +0200

    add axios dependency and change spaces to tabs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,27 +5,27 @@
 </template>
 
 <script>
-	  import axios from 'axios'
-	  export default {
-		  data() {
-			  return {
-				  answer: ''
-			  }
-		  },
-		  mounted () {
-			  axios.get('http://localhost:3000/')
-				  .then(response => this.answer = response.data)
-		  }
-	  }
+		import axios from 'axios'
+		export default {
+			data() {
+				return {
+					answer: ''
+				}
+			},
+			mounted () {
+				axios.get('http://localhost:3000/')
+					.then(response => this.answer = response.data)
+			}
+		}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
