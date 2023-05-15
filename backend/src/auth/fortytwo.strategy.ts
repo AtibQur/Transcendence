decfcf2f30f73f5ec6c122588c2de82a710a863d
rkoper <raavkoper01@outlook.com>
2023-05-15 11:11:21 +0200

    added the right scope to the strategy and found error with callback url

--- backend/src/auth/fortytwo.strategy.ts
+++ backend/src/auth/fortytwo.strategy.ts
@@ -7,9 +7,9 @@ import { Verify } from 'crypto';
 export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
     constructor() {
         super({
-            clientID: 'u-s4t2ud-1f5d67cb202d54f32ab27a0d8d47faa94081df9fc3e7da31b097a09fb7707578',
-            clientSecret: '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',
-            callbackUrl: 'http://localhost:8080/auth/42/callback',
+            clientID : 'u-s4t2ud-1f5d67cb202d54f32ab27a0d8d47faa94081df9fc3e7da31b097a09fb7707578',
+            clientSecret : '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',
+            callBackUrl : 'http://localhost:8080/auth/42/callback',
             scope: 'public',
         });
     }
