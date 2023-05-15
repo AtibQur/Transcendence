941f4d0adb36b89d267e491c40fc80e01cd1f5c7
Haseeb Qureshie <76216657+AtibQur@users.noreply.github.com>
2023-05-15 11:49:30 +0200

    Deleted about info

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,17 +2,12 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import App from './App.vue';
 import Home from './components/Home.vue';
-import About from './components/About.vue';
 import Play from './components/Play.vue';
 
 const routes: RouteRecordRaw[] = [
     {
       path: '/',
       component: Home,
-    },
-    {
-      path: '/about',
-      component: About,
     },
     {
       path: '/play',
