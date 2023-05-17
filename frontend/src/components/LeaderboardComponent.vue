5c3de7737aa4b695b671b19169dbb1eb04311d06
Haseeb Qureshie <76216657+AtibQur@users.noreply.github.com>
2023-05-17 14:30:32 +0200

    Leaderboard footer/img added

--- frontend/src/components/LeaderboardComponent.vue
+++ frontend/src/components/LeaderboardComponent.vue
@@ -2,7 +2,7 @@
     <div class="Leaderboard">
 			<div class="Leaderboard-header">
 				<div class="Leaderboard-logo">
-					<img src="/" alt="Pong-logo">
+					<img src="../assets/images/pongEmoji.png" alt="Pong-logo">
 				</div>
 				<div class="Leaderboard-text">
 					<h1>LEADERBOARD</h1>
@@ -24,11 +24,17 @@
 				</div>
 			</div>
 
-			<!-- <div class="Leaderboard-scores">
-				<div class="Leaderboard-singlescore">
-
+			<div class="Leaderboard-footer">
+				<div class="Leaderboard-player-rank">
+					<h2>hardcoded 1</h2>
 				</div>
-			</div> -->
+				<div class="Leaderboard-player-name">
+					<h2>Hardcoded name</h2>
+				</div>
+				<div class="Leaderboard-player-games">
+					<h2>Hardercoded games</h2>
+				</div>
+			</div>
 
     </div>
 </template>
@@ -170,12 +176,79 @@
 	.Leaderboard-info .Leaderboard-games h2 {
 		font-family: 'JetBrains Mono';
 		font-style: normal;
-		/* line-height: 127px; */
 		color: #595959;
 		font-weight: 400;
 		font-size: 14px;
 		padding-top: 6%;
 	}
-
 	
+	.Leaderboard-footer {
+		position: absolute;
+		top: 90%;
+		left: 0;
+		width: 100%;
+		height: 10%;
+		background-color: #1D233A;
+		border-top: 2px solid black;
+	}
+	.Leaderboard-footer .Leaderboard-player-rank {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 20%;
+		height: 100%;
+		border-right: 2px solid black;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.Leaderboard-footer .Leaderboard-player-rank h2 {
+		font-family: 'JetBrains Mono';
+		font-style: normal;
+		line-height: 127px;
+		color: #FAFAFA;
+		font-weight: 400;
+		font-size: 14px;
+		padding-top: 6%;
+	}
+	.Leaderboard-footer .Leaderboard-player-name {
+		position: absolute;
+		top: 0;
+		left: 20%;
+		width: 60%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.Leaderboard-footer .Leaderboard-player-name h2 {
+		font-family: 'JetBrains Mono';
+		font-style: normal;
+		line-height: 127px;
+		color: #FAFAFA;
+		font-weight: 400;
+		font-size: 14px;
+		padding-left: 6%;
+		padding-top: 3%;
+	}
+	.Leaderboard-footer .Leaderboard-player-games {
+		position: absolute;
+		top: 0;
+		left: 80%;
+		width: 20%;
+		height: 100%;
+		border-left: 2px solid black;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.Leaderboard-footer .Leaderboard-player-games h2 {
+		font-family: 'JetBrains Mono';
+		font-style: normal;
+		color: #FAFAFA;
+		font-weight: 400;
+		font-size: 14px;
+		padding-top: 6%;
+	}
+
 	</style>
