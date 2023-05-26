0b6322b00fec2bc20677e87663625a6cd0b8b1a8
Haseeb Qureshie <76216657+AtibQur@users.noreply.github.com>
2023-05-26 12:17:49 +0200

    Leaderboard is showing json player info

--- frontend/src/components/leaderboard/LeaderboardComponent.vue
+++ frontend/src/components/leaderboard/LeaderboardComponent.vue
@@ -40,66 +40,62 @@
 
 <script setup lang="ts">
 
-	// import { Name } from './LeaderboardPlayer.vue';
-	// import { Wins } from './LeaderboardPlayer.vue';
-	// import { Rank } from './LeaderboardPlayer.vue';
+	// Footer player info
 
-	// // Footer player info
+	// imports
+	import axios from 'axios';
+	import { onBeforeMount, onMounted, ref } from 'vue';
 
-	// // imports
-	// import axios from 'axios';
-	// import { onBeforeMount, onMounted, ref } from 'vue';
+	// get the name from the backend
+	const Name = ref("");
+	const NameLoaded = ref(false);
 
-	// // get the name from the backend
-	// const Name = ref("");
-	// const NameLoaded = ref(false);
+	//functions
+	async function fetchName() {
+		try {
+			const response = await axios.get('http://localhost:3000/Leaderboard/name');
+			Name.value = response.data;
+			NameLoaded.value = true
+		} catch (error) {
+			console.log("Error occured");
+		}
+	}
 
-	// //functions
-	// async function fetchName() {
-	// 	try {
-	// 		const response = await axios.get('http://localhost:3000/Leaderboard/name');
-	// 		Name.value = response.data;
-	// 		NameLoaded.value = true
-	// 	} catch (error) {
-	// 		console.log("Error occured");
-	// 	}
-	// }
+	// get the wins from the backend
+	const Wins = ref("");
+	const WinsLoaded = ref(false);
 
-	// // get the wins from the backend
-	// const Wins = ref("");
-	// const WinsLoaded = ref(false);
+	//functions
+	async function fetchWins() {
+		try {
+			const response = await axios.get('http://localhost:3000/Leaderboard/wins');
+			Wins.value = response.data;
+			WinsLoaded.value = true
+		} catch (error) {
+			console.log("Error occured");
+		}
+	}
 
-	// //functions
-	// async function fetchWins() {
-	// 	try {
-	// 		const response = await axios.get('http://localhost:3000/Leaderboard/wins');
-	// 		Wins.value = response.data;
-	// 		WinsLoaded.value = true
-	// 	} catch (error) {
-	// 		console.log("Error occured");
-	// 	}
-	// }
+	// get the Rank from the backend
+	const Rank = ref("");
+	const RankLoaded = ref(false);
 
-	// // get the Rank from the backend
-	// const Rank = ref("");
-	// const RankLoaded = ref(false);
-
-	// //functions
-	// async function fetchRank() {
-	// 	try {
-	// 		const response = await axios.get('http://localhost:3000/Leaderboard/Rank');
-	// 		Rank.value = response.data;
-	// 		RankLoaded.value = true
-	// 	} catch (error) {
-	// 		console.log("Error occured");
-	// 	}
-	// }
+	//functions
+	async function fetchRank() {
+		try {
+			const response = await axios.get('http://localhost:3000/Leaderboard/Rank');
+			Rank.value = response.data;
+			RankLoaded.value = true
+		} catch (error) {
+			console.log("Error occured");
+		}
+	}
 	
-	// onBeforeMount(() => {
-	// 	fetchName();
-	// 	fetchWins();
-	// 	fetchRank();
-	// })
+	onBeforeMount(() => {
+		fetchName();
+		fetchWins();
+		fetchRank();
+	})
 
 </script>
 
