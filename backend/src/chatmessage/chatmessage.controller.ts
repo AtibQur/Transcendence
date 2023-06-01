db69e13afc3c25ab8623161ce49f78271a7b4323
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2023-06-01 13:43:44 +0200

    add find all msgs within channel func and remove unnecessary stuff

--- backend/src/chatmessage/chatmessage.controller.ts
+++ backend/src/chatmessage/chatmessage.controller.ts
@@ -1,34 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ChatmessageService } from './chatmessage.service';
 import { CreateChatmessageDto } from './dto/create-chatmessage.dto';
-import { UpdateChatmessageDto } from './dto/update-chatmessage.dto';
 
 @Controller('chatmessage')
 export class ChatmessageController {
   constructor(private readonly chatmessageService: ChatmessageService) {}
 
+  // CREATE NEW CHAT MESSAGE
   @Post('create')
   create(@Body() createChatmessageDto: CreateChatmessageDto) {
     return this.chatmessageService.createChatMessage(createChatmessageDto);
   }
 
-  @Get()
-  findAll() {
-    return this.chatmessageService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.chatmessageService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChatmessageDto: UpdateChatmessageDto) {
-    return this.chatmessageService.update(+id, updateChatmessageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.chatmessageService.remove(+id);
+  // GET ALL CHAT MESSAGES WITHIN ONE CHANNEL
+  @Get(':channel_id')
+  findChannelMsgs(@Param('channel_id') channel_id: string) {
+    return this.chatmessageService.findChannelMsgs(+channel_id);
   }
 }
--- backend/src/chatmessage/dto/create-chatmessage.dto.ts
+++ backend/src/chatmessage/dto/create-chatmessage.dto.ts
@@ -2,5 +2,4 @@ export class CreateChatmessageDto {
     content: string;
     sender_id: number;
     channel_id: number;
-    sent_at: Date;
   }
