843158be38894c92a0d92291c3a238100201fd32
Haseeb Qureshie <hqureshi@f1r4s3.codam.nl>
2023-06-06 14:48:15 +0200

    Added ProfileAchievements and Profilestats pages

--- frontend/src/components/profile/ProfileAchievements.vue
+++ frontend/src/components/profile/ProfileAchievements.vue
@@ -1,17 +1,119 @@
 <template>
-<h1>Achievementhjbbs</h1>
+  <div class="scrollable-container">
+    <div class="content">
+      <ul class="item-list">
+        <li v-for="item in items" :key="item" class="item">
+          <div class="image-container">
+            <img :src="getImageForItem(item)" alt="Item Image">
+          </div>
+          <div class="name-container">
+            <div class="item-name">{{ item }}</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<style>
+.scrollable-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  margin-top: 30px;
+  width: 150%;
+  height: 800px;
+  border: 2px solid #131780;
+  border-radius: 8px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background-color: aliceblue;
+}
+
+.content {
+  width: 100%;
+}
+
+.item-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  border: 1px solid #131780;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  padding: 10px;
+  margin-left: 2px;
+  flex-basis: 30%;
+  transition: background-color 0.3s ease;
+}
+
+.item:hover {
+  background-color: lightgray;
+}
+
+.image-container {
+  width: 80px;
+  height: 80px;
+  margin-right: 10px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+}
+
+.image-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.name-container {
+  flex: 1;
+}
+
+.item-name {
+  text-align: center;
+}
+</style>
 
 <script>
 export default {
-  name: "ProfileAchievements",
   data() {
     return {
-      selectedOption: "Achievements",
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+        'Item 5',
+        'Item 6',
+        'Item 7',
+        'Item 8',
+        'Item 9',
+        'Item 10',
+        'Item 11',
+        'Item 12',
+        'Item 13',
+        'Item 14',
+        'Item 15',
+        'Item 16',
+        'Item 17',
+        'Item 18',
+        'Item 19',
+        'Item 20',
+      ],
     };
+  },
+  methods: {
+    getImageForItem(item) {
+      // Add logic here to return the image source for each item
+      // You can use a mapping or generate random images based on the item
+      // For simplicity, let's assume the image filenames follow a pattern
+      return `path/to/images/${item.toLowerCase().replace(/\s+/g, '-')}.jpg`;
+    },
   },
 };
 </script>
-
-<style>
-</style>
--- frontend/src/components/profile/ProfileStats.vue
+++ frontend/src/components/profile/ProfileStats.vue
@@ -1,17 +1,98 @@
 <template>
-    <h1>Stats</h1>
-    </template>
-    
-    <script>
-    export default {
-      name: "ProfileStats",
-      data() {
-        return {
-          selectedOption: "Stats",
-        };
-      },
+  <div class="border-container">
+    <div class="border">
+      <div class="border-text">
+        <div class="border-row">
+          <div class="border-label">Total Games:</div>
+          <div class="border-value">{{ totalGames }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Wins:</div>
+          <div class="border-value">{{ wins }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Loses:</div>
+          <div class="border-value">{{ loses }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Win/Lose Ratio:</div>
+          <div class="border-value">{{ winLoseRatio }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Current Win Streak:</div>
+          <div class="border-value">{{ currentWinStreak }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Highest Win Streak:</div>
+          <div class="border-value">{{ highestWinStreak }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Current Ladder Level:</div>
+          <div class="border-value">{{ currentLadderLevel }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">All-time High:</div>
+          <div class="border-value">{{ allTimeHigh }}</div>
+        </div>
+        <div class="border-row">
+          <div class="border-label">Total Achievements:</div>
+          <div class="border-value">{{ totalAchievements }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      totalGames: 0,
+      wins: 0,
+      loses: 0,
+      winLoseRatio: 0,
+      currentWinStreak: 0,
+      highestWinStreak: 0,
+      currentLadderLevel: '',
+      allTimeHigh: '',
+      totalAchievements: 0
     };
-    </script>
-    
-    <style>
-    </style>
+  }
+};
+</script>
+
+<style>
+.border-container {
+  height: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.border {
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.border-text {
+  font-family: Arial, sans-serif;
+  font-size: 18px;
+}
+
+.border-row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.border-label {
+  font-weight: bold;
+}
+
+.border-value {
+  margin-left: 10px;
+}
+</style>
