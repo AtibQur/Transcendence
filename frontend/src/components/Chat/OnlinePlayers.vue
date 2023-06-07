c1d97fdce705db7793c203648869d61aa050f36a
Tessa Van der vorst <tessavdvorst@hotmail.com>
2023-06-07 14:05:03 +0200

    add (you) to online players display

--- frontend/src/components/Chat/OnlinePlayers.vue
+++ frontend/src/components/Chat/OnlinePlayers.vue
@@ -23,19 +23,10 @@ const props = defineProps({
     }
 });
 
-/* 
-    1. at the start all channels of a player are displayed. -> findPlayerChannels()
-        - with the player id, an object is returned with the channel_id,
-        - for each channel_id, the name needs to be found -> findOneChannelName()
-    2. component is also listening whether there is a new channel added for the current player -> addChannel()
-    3. component is also listening whether the player leaves a channels -> leaveChannel()
-*/
-
 onBeforeMount(() => {
-    socket.emit('findUsername'), props.playerId, (name: string) => {
-        console.log(name);
-        // currentPlayer.value = name;
-    }
+    socket.emit('findUsername', props.playerId, (name: string) => {
+        currentPlayer.value = name;
+    });
 
     // FIND ALL CHANNEL FOR PLAYER
     socket.emit('findAllOnlinePlayers', (response) => {
@@ -44,13 +35,9 @@ onBeforeMount(() => {
         });
     });
 
-    //UPDATE LIST OF CHANNELS IF NEW CHANNEL IS ADDED
+    //UPDATE LIST OF ONLINE PLAYERS IF NEW PLAYER IS ADDED
     // socket.on('player', (payload: {channel_id: number}) => {
     //     channels.value.push({ channel_id: payload.channel_id });
-    // });
-
-    // socket.on('leftChannel', (channel_id) => {
-
     // });
 
 })
