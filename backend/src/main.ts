6e53746e66753969f20d63756cfffcdde8dd556d
Maria Daan <mariadaan@gmail.com>
2023-06-28 18:02:34 +0200

    undo storage in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 import * as passport from 'passport';
-import localStoragePlugin from './localStoragePlugin.ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(localStoragePlugin);
   app.enableCors();
   app.use(
     session({
@@ -21,10 +19,10 @@ async function bootstrap() {
     )
     app.use(passport.initialize());
     app.use(passport.session());
-    
-    app.enableCors( {
-      origin: ['http://localhost:8080']
-    });
+
+  app.enableCors( {
+	origin: ['http://localhost:8080']
+  });
   await app.listen(3000);
 }
 bootstrap();
