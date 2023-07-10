6e141195002d0d898efdf188150ef900f56f4043
Tessa <Tessa@mbp-van-sabine.kpn>
2023-07-10 13:20:54 +0200

    change online player display with axios call

--- frontend/src/components/Chat/OnlinePlayers.vue
+++ frontend/src/components/Chat/OnlinePlayers.vue
@@ -12,7 +12,7 @@
 <script setup lang="ts">
 import { socket } from '../../socket';
 import axiosInstance from '../../axiosConfig';
-import { onBeforeMount, ref, computed } from 'vue'
+import { onBeforeMount, ref} from 'vue'
 
 const onlinePlayers = ref([]);
 const playerId = parseInt(sessionStorage.getItem('playerId') || '0');
@@ -20,12 +20,13 @@ const currentPlayer = sessionStorage.getItem('username');
 
 onBeforeMount(async () => {
 
-    // FIND ALL CHANNEL FOR PLAYER
-    socket.emit('findAllOnlinePlayers', (response) => {
-        response.forEach(user => {
-            onlinePlayers.value.push(user.player.username);
-        });
-    });
+    // FIND ALL ONLINE Players
+    const fetchOnlinePlayers = async () => {
+    const response = await axiosInstance.get('player/online');
+    onlinePlayers.value = response.data.map(item => item.player.username);
+    }
+
+    await fetchOnlinePlayers();
 
     //UPDATE LIST OF ONLINE PLAYERS IF NEW PLAYER IS ADDED
     // socket.on('player', (player_id: number) => {
@@ -35,32 +36,5 @@ onBeforeMount(async () => {
 
 })
 
-// EVENT TO CHANGE CURRENT CHANNEL
-// const changeChannel = (channel_id: number) => {
-//     emit('changeChannel', channel_id);
-// }
-
-// const fetchChannelName = async (channel_id: number) => {
-
-//     return new Promise<string>((resolve) => {
-//         socket.emit('findOneChannelName', channel_id, (channel_name: string) => {
-//             resolve(channel_name);
-//         });
-//     });
-// };
-
-// const updateChannelName = async (channel_id: number) => {
-//     const channel_name = await fetchChannelName(channel_id);
-//     channelNames.value[channel_id] = channel_name;
-// };
-
-// const getChannelName = (channel_id: number) => {
-//     if (channelNames.value[channel_id]) {
-//         return channelNames.value[channel_id];
-//     }
-    
-//     updateChannelName(channel_id);
-//     return computed(() => channelNames.value[channel_id]);
-// };
 
 </script>
