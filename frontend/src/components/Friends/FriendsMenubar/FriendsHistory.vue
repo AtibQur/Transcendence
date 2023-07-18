9eafbb0b4c72f4d382a9009e64a24ad82962555b
Haseeb Qureshie <76216657+AtibQur@users.noreply.github.com>
2023-07-18 16:25:21 +0200

    Added some profile/friends history information

--- frontend/src/components/Friends/FriendsMenubar/FriendsHistory.vue
+++ frontend/src/components/Friends/FriendsMenubar/FriendsHistory.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="border-container">
     <div class="border">
+      <div class="border-match-info">
+        <div class="user-info">
+          <div class="player-infoRow">
+            <div class="user-infoName">player</div>
+            <div class="user-infoScore">score</div>
+            <div class="enemy-infoScore">score</div>
+            <div class="enemy-infoName">enemy</div>
+          </div>
+        </div>
+      </div>
+
       <div class="border-text">
         <div v-for="(match, index) in matches" :key="index" class="border-row">
-          <div class="border-value">{{ match.player.username }} {{ match.player_points }}</div>
-          <div class="border-value">{{ match.opponent_points }} {{ match.opponent.username }}</div>
+          <div class="border-value">{{ match.player.username }}</div>
+          <div class="border-value">{{ match.player_points }}</div>
+          <div class="border-value">{{ match.opponent_points }}</div>
+          <div class="border-value">{{ match.opponent.username }}</div>
         </div>
       </div>
     </div>
@@ -44,7 +57,9 @@ const useFriendsHistory = (friendId) => {
 
   const fetchMatches = async (friendId) => {
     const response = await axiosInstance.get(`match/history/` + friendId.toString());
-    return response.data;
+    const reversedMatches = response.data.reverse(); // Reverse the order of matches
+    const latestMatches = reversedMatches.slice(0, 13); // Fetch the latest 12 matches
+    return latestMatches;
   };
 
   return {
@@ -55,4 +70,45 @@ const useFriendsHistory = (friendId) => {
 </script>
 
 <style>
+.border-match-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px; /* Add margin-bottom to match the spacing between matches */
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px; /* Add margin-bottom to match the spacing between matches */
+}
+
+.player-infoRow {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  margin-left: 50px;
+  text-align: center;
+
+}
+
+.player-infoRow > *:not(:last-child) {
+  margin-right: 100px;
+}
+
+
+.border .border-row {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  margin-bottom: 8px; /* Adjust the spacing between matches if needed */
+  padding: 10px; /* Adjust the padding within each match border if needed */
+}
+
+.border .border-value {
+  margin-bottom: 5px;
+  flex-basis: 25%; /* Adjust the width of each value as needed */
+}
+
 </style>
--- frontend/src/components/profile/ProfileHistory.vue
+++ frontend/src/components/profile/ProfileHistory.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="border-container">
-      <div class="border">
+  <div class="border-container">
+    <div class="border">
+      <div class="border-match-info">
+        <div class="user-info">
+          <div class="player-infoRow">
+            <div class="user-infoName">player</div>
+            <div class="user-infoScore">score</div>
+            <div class="enemy-infoScore">score</div>
+            <div class="enemy-infoName">enemy</div>
+          </div>
+        </div>
+      </div>
+
       <div class="border-text">
         <div v-for="(match, index) in matches" :key="index" class="border-row">
-          <div class="border-value">{{ match.player.username }} {{ match.player_points }} </div>
-          <div class="border-value">{{ match.opponent_points }} {{ match.opponent.username }} </div>
+          <div class="border-value">{{ match.player.username }}</div>
+          <div class="border-value">{{ match.player_points }}</div>
+          <div class="border-value">{{ match.opponent_points }}</div>
+          <div class="border-value">{{ match.opponent.username }}</div>
         </div>
       </div>
     </div>
@@ -12,26 +25,69 @@
 </template>
 
 <script setup lang="ts">
-  import { onBeforeMount, ref } from 'vue';
-  import axiosInstance from '../../axiosConfig';
+import { onBeforeMount, ref } from 'vue';
+import axiosInstance from '../../axiosConfig';
 
-  const matches = ref("");
-  const playerId = parseInt(localStorage.getItem('playerId') || '0');
+const matches = ref([]);
+const playerId = parseInt(localStorage.getItem('playerId') || '0');
 
-  onBeforeMount(async () => {
+onBeforeMount(async () => {
   try {
     matches.value = await fetchMatches(playerId);
     console.log(matches.value);
   } catch (error) {
-    console.log("Error occured");
+    console.log("Error occurred:", error);
   }
-  });
+});
 
-  const fetchMatches = async (player_id: number) => {
-    const response = await axiosInstance.get('match/history/' + player_id.toString());
-    return response.data;
-  }
+const fetchMatches = async (playerId: number) => {
+  const response = await axiosInstance.get('match/history/' + playerId.toString());
+  const reversedMatches = response.data.reverse(); // Reverse the order of matches
+  const latestMatches = reversedMatches.slice(0, 13); // Fetch the latest 12 matches
+  return latestMatches;
+};
 </script>
 
 <style>
+.border-match-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px; /* Add margin-bottom to match the spacing between matches */
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px; /* Add margin-bottom to match the spacing between matches */
+}
+
+.player-infoRow {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  margin-left: 50px;
+  text-align: center;
+
+}
+
+.player-infoRow > *:not(:last-child) {
+  margin-right: 100px;
+}
+
+
+.border .border-row {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  margin-bottom: 8px; /* Adjust the spacing between matches if needed */
+  padding: 10px; /* Adjust the padding within each match border if needed */
+}
+
+.border .border-value {
+  margin-bottom: 5px;
+  flex-basis: 25%; /* Adjust the width of each value as needed */
+}
+
 </style>
