4712f6d42130accb05869377e43c3e83982345df
Tessa Van der vorst <tessavdvorst@hotmail.com>
2023-07-24 14:23:20 +0200

    removed auto connect in socket

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
   
 export const socket = io('http://localhost:3000', { 
-    autoConnect: false, 
     auth: {
         id: sessionStorage.getItem('playerId') || '0'
     }
