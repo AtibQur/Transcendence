0ca8c29758ac26df2bd7135998491a5f3331b5fe
Maria Daan <mariadaan@gmail.com>
2023-07-27 12:14:47 +0200

    use JetBrains font throughout full application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,8 +17,9 @@
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=JetBrains+Mono');
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'JetBrains Mono';
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
