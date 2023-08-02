710dd789278af21d0d54003b98f48f357164a15a
Haseeb Qureshie <76216657+AtibQur@users.noreply.github.com>
2023-08-02 14:51:47 +0200

    Chat scrollbar is always down, max height limit for the chat

--- frontend/src/components/Chat/ChatBox.vue
+++ frontend/src/components/Chat/ChatBox.vue
@@ -3,86 +3,77 @@
       <button class="channel-name-button" @click="showInfo()"> 
         <b>{{ channelName }}</b>
       </button>
-      <div class="messages-container">
+      <div class="messages-container" ref="messagesContainerRef">
         <div v-for="message in messages" :key="message.id" :class="getMessageSenderClass(message)">
           {{ message.sender.username }}
           <div :class="getMessageClass(message)">
             {{ message.content }}
-        </div>
           </div>
+        </div>
       </div>
     </div>
   </template>
-
-<script setup lang="ts">
-import { socket } from '@/socket';
-import axiosInstance from '../../axiosConfig';
-import { onBeforeMount, onUpdated, ref, computed, watch} from 'vue'
-import Message from '@/types/Message';
-
-const props = defineProps({
+  
+  <script setup lang="ts">
+  import { socket } from '@/socket';
+  import axiosInstance from '../../axiosConfig';
+  import { onBeforeMount, ref, watch, nextTick } from 'vue'
+  import Message from '@/types/Message';
+  
+  const props = defineProps({
     channelId: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true
     }
-});
-
-const emit = defineEmits(['showInfo']);
-const channelName = ref('');
-const messages = ref<Message[]>([]);
-const currentChannelId = ref(props.channelId);
-// const newDate = ref(true);
-const playerId = parseInt(sessionStorage.getItem('playerId') || '0');
-const username = sessionStorage.getItem('username') || '0';
-// const dates = ref<string>([]);
-
-onBeforeMount(async () => {
+  });
+  
+  const emit = defineEmits(['showInfo']);
+  const channelName = ref('');
+  const messages = ref<Message[]>([]);
+  const currentChannelId = ref(props.channelId);
+  const messagesContainerRef = ref<HTMLElement | null>(null);
+  const playerId = parseInt(sessionStorage.getItem('playerId') || '0');
+  const username = sessionStorage.getItem('username') || '0';
+  
+  onBeforeMount(async () => {
     await fetchChatMessagesFiltered(playerId, currentChannelId.value);
     await fetchChannelName(currentChannelId.value);
-
-    
-    //ADD MESSAGE TO CURRENT MESSAGES
+  
     socket.on('chatmessage', (message: Message) => {
-        console.log("new message");
-        addChatmessage(message);
+      console.log("new message");
+      addChatmessage(message);
     });
-
-    //TRACK WHETHER CHANNEL_ID CHANGES
+  
     watch(() => props.channelId, async (newChannelId) => {
-        currentChannelId.value = newChannelId;
-        await fetchChatMessagesFiltered(playerId, currentChannelId.value);
-        await fetchChannelName(currentChannelId.value);
+      currentChannelId.value = newChannelId;
+      await fetchChatMessagesFiltered(playerId, currentChannelId.value);
+      await fetchChannelName(currentChannelId.value);
     });
-    
-});
-
-// FIND CHANNEL MESSAGES FILTERED
-const fetchChatMessagesFiltered = async (player_id: number, channel_id: number) => {
+  });
+  
+  const fetchChatMessagesFiltered = async (player_id: number, channel_id: number) => {
     const channel_id_query = 'channel_id=' + channel_id.toString();
     const response = await axiosInstance.get('chatmessage/filtered/' + player_id.toString() + `?${channel_id_query}`);
     messages.value = response.data;
-};
-
-//FIND CHANNEL NAME
-const fetchChannelName = async (channelId: number) => {
+  };
+  
+  const fetchChannelName = async (channelId: number) => {
     const response = await axiosInstance.get('channel/' + channelId.toString());
-    if (response.data)
-    {
-        if (!response.data.is_dm)
-            channelName.value = response.data.name;
-        else { //if the channel is a dm then channelname become the name of the friend
-            const members = await axiosInstance.get('channelmember/allmembers/' + channelId.toString());
-            if (members.data)
-            {
-                const friend = members.data.find((member) => member.member_id !== playerId);
-                if (friend)
-                    channelName.value = friend.member.username;
-            }
+    if (response.data) {
+      if (!response.data.is_dm)
+        channelName.value = response.data.name;
+      else {
+        const members = await axiosInstance.get('channelmember/allmembers/' + channelId.toString());
+        if (members.data) {
+          const friend = members.data.find((member) => member.member_id !== playerId);
+          if (friend)
+            channelName.value = friend.member.username;
         }
-    }
-    else
-        console.log('Error fetching channelname');
-};
+      }
+    } else
+      console.log('Error fetching channelname');
+  };
+  
 
 const getMessageClass = (message: Message) => {
   return message.sender.username === username ? 'my-message' : 'friend-message';
@@ -133,17 +124,29 @@ const showInfo = () => {
 // }
 
 async function addChatmessage(message: Message) {
-    try {
-        const username_query = 'username=' + message.sender.username;
-        const player_id = sessionStorage.getItem('playerId');
-        console.log(player_id);
-        const isBlocked = await axiosInstance.get('blockedplayer/player/' + player_id + `?${username_query}`);
-        // if (!isBlocked)
-        messages.value.push(message);
-    } catch (error) {
-        console.log('Error: adding message');
-    }
+  try {
+    const username_query = 'username=' + message.sender.username;
+    const player_id = sessionStorage.getItem('playerId');
+    const isBlocked = await axiosInstance.get('blockedplayer/player/' + player_id + `?${username_query}`);
+    messages.value.push(message);
+    nextTick(scrollToBottom); // Scroll to bottom when a new message is added
+  } catch (error) {
+    console.log('Error: adding message');
+  }
 }
+
+// Scroll to the bottom of the messages container
+function scrollToBottom() {
+  const container = messagesContainerRef.value;
+  if (container) {
+    container.scrollTop = container.scrollHeight;
+  }
+}
+
+// Watch for changes in the messages array and scroll to the bottom when needed
+watch(messages, () => {
+  nextTick(scrollToBottom);
+}, { immediate: true });
 
 </script>
 
@@ -179,6 +182,8 @@ ul {
 .messages-container {
     display: flex;
     flex-direction: column;
+    max-height: 70vh; /* Adjust the maximum height relative to viewport height */
+    overflow-y: auto;
   }
 
 .my-message-sender {
